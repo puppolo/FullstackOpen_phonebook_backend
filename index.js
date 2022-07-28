03a6bfbd5c9bfc2fded1777bb2047bb336ccc1a9
puppolo <nontanat.nutsati@gmail.com>
2022-07-28 10:15:38 +0700

    finish ex 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+    ].join(' ')
+  }))
+
+//app.use(express.json())
 
 let persons = [
     { 
